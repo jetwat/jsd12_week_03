cac1f5811a535817f6cea88c3bd9b1259f2907ed
jetwat <jetwatwarathip@gmail.com>
2026-03-19 17:11:53 +0700

    added learning files about JavaScript functions

---
+++ 04_js-functions/01_declarative-functions.js
@@ -0,0 +1,20 @@
+function greet(name, role2) {
+  // write function logic here
+  const role = "Coder";
+  // set return value of this function
+  return `${role2 + " " + name} is a ${role}.`;
+}
+
+// "function" keyword
+// function name
+// bracket () syntax
+// bracket {} syntax
+// "return" keyword
+
+greet(); // calling this function returns "Hello world!"
+const role = "Not Coder";
+const role2 = "Someone name, ";
+console.log(greet("Neeti", "Mister"));
+
+/*--------------------------------------------------------------------*/
+// function () {}
---
+++ 04_js-functions/02_arrow-functions.js
@@ -0,0 +1,13 @@
+// arrow function was introduced in ES6, ECMAScript
+
+// anonymous arrow function
+() => {};
+
+// arrow function
+const greetUser = (name) => {
+  return `Hello, my name is ${name}`;
+};
+
+greetUser("Chong");
+
+console.log(greetUser("Chong"));
